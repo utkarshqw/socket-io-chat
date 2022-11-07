4dde5ed8bd6a58e114dfdc2961f796fe73380eb7
Utkarsh Gupta <101376775+utkarshqw@users.noreply.github.com>
2022-11-07 23:33:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const server = app.listen(5000, async () => {
 
 const io = require("socket.io")(server,{
   pingTimeout:60000,
-  cors: { origin: "https://nem201-chat-app.vercel.app/", }
+  cors: { origin: "https://nem201-chat-app.vercel.app", }
 })
 
 
